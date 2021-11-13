c9739e30c2d4585bb09b7b7a375b0c9673070dff
Kai0802 <h.phuoc137@gmail.com>
2021-11-13 11:57:41 -0500

    First commit

---
+++ src/nguyen/FinalProjectCS142LatestVersion.java
@@ -0,0 +1,19 @@
+package nguyen;
+
+import javafx.application.Application;
+
+/**
+ * This is just a test comment
+ * 
+ * @author Phuoc Nguyen {@literal <h.phuoc137@gmail.com>}
+ */
+public class FinalProjectCS142LatestVersion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(ProjectUI.class);
+    }
+    
+}
